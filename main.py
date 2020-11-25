108fbc0ad13a8ad9c5ab1e53836c709ccd20d0e6
Simon Hermansson <simhe502@student.liu.se>
2020-11-25 14:16:29 +0100

    More pygame code.

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import math
+import pygame
 
 
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def game_loop():
+    pass
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pygame.init()
+    gameDisplay = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Pool")
+    clock = pygame.time.Clock()
+
+    game_loop()
+    running = True
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.BUTTON_LEFT:
+                pass
+
+        pygame.display.update()
+        clock.tick(60)
